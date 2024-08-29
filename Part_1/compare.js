17dfa823cb7b4503de17f6659d7bbb6a6fcf31c1
Nilesh410 <shirudenileshs@gmail.com>
2024-08-29 09:44:29 +0530

    diff bet == and ===

---
+++ Part_1/compare.js
@@ -0,0 +1,7 @@
+let a=10
+let s="10"
+console.log(a==s)
+console.log(a===s)
+/* What is the difference between == and === in JavaScript?
+Answer: == compares values for equality with type coercion, while === compares values and types strictly. 
+Use === to avoid unexpected type conversions. */
